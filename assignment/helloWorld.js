997daa5fb867dba314862d8e002ebb15dffaa6ac
Janna Luiza Morris <janna.luiza@gmail.com>
2023-08-16 16:38:30 -0500

    First and only commit

---
+++ assignment/helloWorld.js
@@ -0,0 +1,36 @@
+console.log('hello world!');
+let firstName = 'Janna';
+console.log(firstName);
+let lastName = 'Morris';
+console.log(lastName);
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+let temperature = 80;
+console.log(temperature);
+let loggedIn = true;
+console.log('loggedIn', loggedIn);
+let isRaining = false;
+if (loggedIn)  {
+console.log('Welcome ', fullName);}
+else {
+console.log('Welcome, Guest!');
+}
+if (temperature < 85 && !isRaining) {
+console.log('Go for a walk!');
+} else if (isRaining) {
+console.log('Bring an umbrella');
+} else {
+console.log('Better to stay inside');
+}
+temperature = temperature + 1;
+temperature -+ 2;
+temperature *= 2;
+console.log('It is now', temperature);
+if (temperature === 162) {
+console.log('it is 162!');
+}
+
+    
+
+         
+    
